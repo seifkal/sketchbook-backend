7904bf550ae513320b3bfe108a1de6be2dd96ccd
Seifkall <saifkall5@gmail.com>
2025-09-27 10:18:19 +0300

    Initial commit

---
+++ src/main/java/com/sketchbook/sketchbook_backend/SketchbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sketchbook.sketchbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SketchbookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SketchbookBackendApplication.class, args);
+	}
+
+}
