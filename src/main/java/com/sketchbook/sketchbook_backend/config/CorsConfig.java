ec8caf9b0291bd534424f24aff77dede619d1169
Seifkall <saifkall5@gmail.com>
2025-12-21 19:43:33 +0300

    add missing package decleration to CorsConfig

--- src/main/java/com/sketchbook/sketchbook_backend/config/CorsConfig.java
+++ src/main/java/com/sketchbook/sketchbook_backend/config/CorsConfig.java
@@ -1,3 +1,4 @@
+package com.sketchbook.sketchbook_backend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
